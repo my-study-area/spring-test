eb4d6a73be1a5011c55c1f29deae7115252023f7
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-06 16:01:13 -0300

    adiciona teste para soma em Calculadora

---
+++ src/test/java/br/com/springtests/springTests/service/CalculadoraTest.java
@@ -0,0 +1,26 @@
+package br.com.springtests.springTests.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void deveRetornar5QuandoSomar4E1() {
+        assertThat(Calculadora.soma(2, 3), is(5));
+    }
+    
+    @Test
+    public void deveRetornar6QuandoSomar4E2() {
+        assertThat(Calculadora.soma(4, 2), is(6));
+    }
+    
+    @Test
+    public void testar() {
+        assertThat(Calculadora.soma(4, 2), allOf(is(6), not(0)));
+    }
+
+}
