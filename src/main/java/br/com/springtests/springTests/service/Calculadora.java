8dce5c398418e04cdb3022a0c1d479daf9ab1201
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-06 16:03:10 -0300

    adicoina a implementaão da soma em Calculadora

---
+++ src/main/java/br/com/springtests/springTests/service/Calculadora.java
@@ -0,0 +1,11 @@
+package br.com.springtests.springTests.service;
+
+public class Calculadora {
+
+    private Calculadora() {}
+
+    public static int soma(int number1, int number2) {
+        return number1 + number2;
+    }
+
+}
