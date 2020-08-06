f8a663e1cb72811c8be4c082708139a3448beaca
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-05 21:07:20 -0300

    inicia controle de versão

---
+++ src/main/java/br/com/springtests/springTests/SpringTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.springtests.springTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestsApplication.class, args);
+	}
+
+}
