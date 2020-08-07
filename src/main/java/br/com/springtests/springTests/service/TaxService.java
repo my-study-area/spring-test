b93e9d160d937f97390bc876e398fa3459f61c5b
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-06 21:56:50 -0300

    adiciona testes unitários do vídeo Spring Boot - Testing basics (Defog Tech)

---
+++ src/main/java/br/com/springtests/springTests/service/TaxService.java
@@ -0,0 +1,20 @@
+package br.com.springtests.springTests.service;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxService {
+    
+    public String getBracket(int income) {
+        if(income < 1000) return "LOW";
+        else if (income < 5000) return "MEDIUM";
+        else return "HEIGHT";
+    }
+    
+    public List<String> allBracket() {
+        return Arrays.asList("LOW", "MEDIUM", "HEIGHT");
+    }
+
+}
---
+++ src/test/java/br/com/springtests/springTests/service/TaxServiceTest.java
@@ -0,0 +1,31 @@
+package br.com.springtests.springTests.service;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TaxServiceTest {
+
+    private TaxService service = new TaxService();
+    
+    @Test
+    public void bracket() {
+        final String taxBracket = service.getBracket(500);
+        assertThat(taxBracket, is(equalTo("LOW")));
+    }
+    
+    @Test
+    public void allBracket() {
+        final List<String> allBrackets = service.allBracket();
+        assertThat(allBrackets, is(not(empty())));
+        assertThat(allBrackets, contains("LOW","MEDIUM", "HEIGHT"));
+    }
+    
+}
